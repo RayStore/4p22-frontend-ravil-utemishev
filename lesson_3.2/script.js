024a881d03118ee4f8b45a05ec5442e67b808299
Raywill <107601109+RayStore@users.noreply.github.com>
2022-10-26 09:07:17 +0300

    attestation_1, lesson_3.1, lesson_3.2

---
+++ lesson_3.2/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const num = 5; // number | +-(2^53 - 1)
+const floatNum = 5.2; // number | +-(2^53 - 1)
+const bigNum = BigInt(9007199254740992); // bigint | число произвольной длины
+const strWithApples = 'Hello, I have apples';
+console.log(typeof strWithApples);
+
+const strDoubleQuote = "String"; // строка в двойных кавычках
+const strSingleQuote = 'String'; // строка в одинарных кавычках
+const strLeftQuote = `String
+Qeewwrwqs
+dssddsl
+weffefe
+sde`;
+const hasApples = true;
+console.log(typeof hasApples);
+let someConstant = null;
+let someVariable;
+console.log(typeof someConstant);
+console.log(typeof someVariable);
+
+
