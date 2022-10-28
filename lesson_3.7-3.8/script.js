b80717962f1ca13eb40d6e6d3977fafb7ca09501
Raywill <107601109+RayStore@users.noreply.github.com>
2022-10-28 21:32:24 +0300

    homework_8

---
+++ lesson_3.7-3.8/script.js
@@ -0,0 +1,119 @@
+'use strict'
+
+// // const firstError = '';
+// // const secondError = '';
+// // const thirdError = '';
+
+// // // const globalError = firstError + secondError + thirdError;
+
+// // if (!(firstError || secondError || thirdError)) {
+// //     console.log('Ошибки нет');
+// // }
+
+// // for (let i = 0; i < 10; i = i + 2) {
+// //     console.log('Good job, Ray', i);
+// // }
+
+// // console.log('after loop');
+
+// // let i = 0;
+// // for (i = 0; i < 10; i++) {
+// //     console.log('Good job, Ray', i);
+// // }
+
+// // console.log(i);
+
+// // let i = 0;
+
+// // for (; i < 10; i++) {
+// //     console.log('Good job, Ray', i);
+
+// // }
+
+// // console.log(i);
+// // let i = 0;
+
+// // for (; i < 10;) {
+// //     console.log('Good job, Ray', i);
+// //     i++;
+// // }
+
+// // console.log(i);
+
+// // for (let i = 0; i < 10; i++) {
+// //     if (i === 5) {
+// //         break;
+// //     }
+// //     console.log('Good job, Ray', i);
+// // }
+
+// // for (let i = 0; i < 10; i++) {
+// //     if (i === 5) {
+// //         continue;
+// //     }
+// //     console.log('Good job, Ray', i);
+// // }
+
+// // let isLoopContinues = true;
+// // let i = 0;
+
+// // while (isLoopContinues) {
+// //     i = Math.random();
+// //     console.log(i);
+
+// //     if (i > 0.5) {
+// //         isLoopContinues = false;
+// //     }
+
+// // }
+
+// let isLoopContinues = true;
+// let i = 0;
+
+// // while (isLoopContinues) {
+// //     i = Math.random();
+// //     console.log(i);
+
+// //     if (i > 0.5) {
+// //         isLoopContinues = false;
+// //     }
+
+// // }
+
+// // do {
+// //     i = Math.random();
+// //     console.log(i);
+
+// //     if (i > 0.5) {
+// //         isLoopContinues = false;
+// //     } else {
+// //         isLoopContinues = true;
+// //     }
+// // } while (isLoopContinues);
+
+// // console.log(i = 10 * Math.random() + 1);
+
+// while (isLoopContinues) {
+//     i = Math.trunc(10 * Math.random() + 1);
+//     console.log(i);
+
+//     if (i === 8) {
+//         isLoopContinues = false;
+//     }
+
+// Function Declaration
+
+// function sayHello() {
+//     console.log('Hello World');
+// }
+// sayHello();
+
+// function Expression
+// const sayHello2 = function () {
+//     console.log('Hello World!');
+// }
+
+// function Expression (lambda - стрелочная функция)
+
+
+
